3424aa3e018ec544791e3094ed1890a9566f4eb0
Osvaldo Valdivia <ovaldovia983@gmail.com>
2025-07-03 13:50:22 -0600

    Indicador de Vista

--- app.py
+++ app.py
@@ -4,23 +4,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    seccion = "inicio"
+    return render_template('index.html', seccion = seccion)
 
 @app.route('/agenda')
 def agenda():
-    return render_template('agenda.html')
+    seccion = "agenda"
+    return render_template('agenda.html', seccion = seccion)
 
 @app.route('/anfitrion')
 def anfitrion():
-    return render_template('anfitrion.html')
+    seccion = "anfitrion"
+    return render_template('anfitrion.html', seccion = seccion)
 
 @app.route('/reseña')
 def reseña():
-    return render_template('reseña.html')
+    seccion = "reseña"
+    return render_template('reseña.html', seccion = seccion)
 
 @app.route('/beneficios')
 def beneficios():
-    return render_template('beneficios.html')
+    seccion = "beneficios"
+    return render_template('beneficios.html', seccion = seccion)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
